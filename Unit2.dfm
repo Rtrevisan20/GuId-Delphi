75720acd129f25ab1e5aa12d2d1a2e58dc2ea6ca
HardDev <rtinformatica.ti@gmail.com>
2022-06-28 08:26:48 -0400

    Primeiro commit

---
+++ Unit2.dfm
@@ -0,0 +1,35 @@
+object Form2: TForm2
+  Left = 0
+  Top = 0
+  Caption = 'GuID - Delphi'
+  ClientHeight = 283
+  ClientWidth = 453
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  Position = poScreenCenter
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 8
+    Top = 8
+    Width = 75
+    Height = 25
+    Caption = 'Criar GUID'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Memo: TMemo
+    Left = 0
+    Top = 40
+    Width = 453
+    Height = 243
+    Align = alBottom
+    TabOrder = 1
+    ExplicitTop = 56
+    ExplicitWidth = 635
+  end
+end
---
+++ Unit2.pas
@@ -0,0 +1,51 @@
+unit Unit2;
+
+interface
+
+uses
+  Winapi.Windows,
+  Winapi.Messages,
+  System.SysUtils,
+  System.Variants,
+  System.Classes,
+  Vcl.Graphics,
+  Vcl.Controls,
+  Vcl.Forms,
+  Vcl.Dialogs,
+  Vcl.StdCtrls, Winapi.ActiveX;
+
+type
+  TForm2 = class(TForm)
+    Button1: TButton;
+    Memo: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    function GuidCreate : string;
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  Memo.Lines.Add(GuidCreate);
+end;
+
+function TForm2.GuidCreate: string;
+var
+  ID: TGUID;
+begin
+
+  Result := '';
+
+  if CoCreateGuid(ID) = S_OK then
+     Result := GUIDToString(ID);
+end;
+
+end.
